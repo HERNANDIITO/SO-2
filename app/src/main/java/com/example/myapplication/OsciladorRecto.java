b6c328b8b8b0d665cd53c0cf966fd426dfe45987
HERNANDIITO <pablohhgg@gmail.com>
2023-11-30 11:02:12 +0100

    oscilador sinusoidal funcionando
    El oscilador sinusoidal ya funciona perfectamente pero todavía hace falta trabajar en la tolerancia a errores

--- app/src/main/java/com/example/myapplication/OsciladorRecto.java
+++ app/src/main/java/com/example/myapplication/OsciladorRecto.java
@@ -5,8 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.math.BigDecimal;
 
 public class OsciladorRecto extends AppCompatActivity{
+
+    private EditText r1Edit, cEdit;
+    private TextView r2Edit, freq;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +24,35 @@ public class OsciladorRecto extends AppCompatActivity{
     public void Back (View view) {
         Intent mainActivity = new Intent(this, MainActivity.class);
         startActivity(mainActivity);
+    }
+
+    public void Clear(View view) {
+        r1Edit = (EditText) findViewById(R.id.r1);
+        r2Edit = (TextView) findViewById(R.id.r2);
+        cEdit = (EditText) findViewById(R.id.c);
+        freq = (TextView) findViewById(R.id.freq);
+
+        r1Edit.setText("");
+        r2Edit.setText("");
+        cEdit.setText("");
+        freq.setText("");
+    }
+
+    public void Suma(View view) {
+        r1Edit = (EditText) findViewById(R.id.r1);
+        r2Edit = (TextView) findViewById(R.id.r2);
+        cEdit = (EditText) findViewById(R.id.c);
+        freq = (TextView) findViewById(R.id.freq);
+
+        double r1 = Double.parseDouble(r1Edit.getText().toString());
+        double r2 = Double.parseDouble(r2Edit.getText().toString());
+        double c = Double.parseDouble(cEdit.getText().toString());
+        double res;
+
+        res = 1 / (2 * 3.14 * r1 * c * (6^(1/2)) );
+        BigDecimal textRes = new BigDecimal(String.valueOf(res)).setScale(4, BigDecimal.ROUND_FLOOR);
+
+        freq.setText(String.valueOf(textRes));
+
     }
 }
--- app/src/main/java/com/example/myapplication/OsciladorSinusoidal.java
+++ app/src/main/java/com/example/myapplication/OsciladorSinusoidal.java
@@ -4,18 +4,69 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.math.BigDecimal;
 
 public class OsciladorSinusoidal extends AppCompatActivity{
+
+    private EditText rEdit, cEdit, r1Edit, freq;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.oscilador_sinusoidal);
+        rEdit = (EditText) findViewById(R.id.r);
+        r1Edit = (EditText) findViewById(R.id.r1);
+        cEdit = (EditText) findViewById(R.id.c);
+        freq = (EditText) findViewById(R.id.freq);
+
+        rEdit.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) { }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) { }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if ( rEdit.getText().toString().isEmpty() ) {
+                    r1Edit.setText("");
+                    return;
+                }
+
+                double r = Double.parseDouble(rEdit.getText().toString());
+                r1Edit.setText(String.valueOf(r * 29));
+            }
+        });
     }
 
     public void Back (View view) {
         Intent mainActivity = new Intent(this, MainActivity.class);
         startActivity(mainActivity);
+    }
+
+    public void Clear(View view) {
+        rEdit.setText("");
+        r1Edit.setText("");
+        cEdit.setText("");
+        freq.setText("");
+    }
+
+    public void Suma(View view) {
+        double r = Double.parseDouble(rEdit.getText().toString());
+        double r1 = Double.parseDouble(r1Edit.getText().toString());
+        double c = Double.parseDouble(cEdit.getText().toString());
+        double denom = 2*Math.PI * r * c * Math.sqrt(6);
+        double res = 1 / denom;
+
+        BigDecimal textRes = new BigDecimal(String.valueOf(res)).setScale(4, BigDecimal.ROUND_FLOOR);
+
+        freq.setText(String.valueOf(textRes));
+
     }
 }
